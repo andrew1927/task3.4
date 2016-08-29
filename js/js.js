03dfafce8688cd99c2fe515419c97ffdaa19adda
Molodetskyi Andrii <amolodetskyi@gmail.com>
2016-08-29 14:36:10 +0300

    update js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,29 @@
-var elText = document.getElementsByClassName('text')[0];
-var elwrapper = document.getElementsByClassName('content-wrapper')[0];
+var elText = document.getElementsByClassName('text')[0],	
+    elPause = document.getElementsByClassName('pause')[0],	
+	elwrapper = document.getElementsByClassName('content-wrapper')[0],
+	play = false,
+	pause = false;
 
-elText.addEventListener( "click", function() 
-	{elwrapper.classList.add("animate");});
-
+elText.addEventListener( "click", function() {
+	if(!play){
+		elwrapper.classList.add("animate");
+		play = true;
+	}
+	else{
+		elwrapper.classList.remove("animate");
+		// elwrapper.classList.add("animate");
+		play = false;
+	}
+});
+elPause.addEventListener( "click", function() {
+	if(!pause){
+		elwrapper.classList.add("animate-pause");
+		pause = true;
+		elPause.innerHTML = "Play";
+	}
+	else{
+		elwrapper.classList.remove("animate-pause");
+		pause = false;
+		elPause.innerHTML = "Pause";
+	}
+});
